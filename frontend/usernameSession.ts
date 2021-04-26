cb6978b2153827c6727f7f73de33c38b4c70096f
Leon von Voss <if20b169@technikum-wien.at>
2021-04-26 14:13:38 +0200

    Fixed Rest service

--- frontend/usernameSession.ts
+++ frontend/usernameSession.ts
@@ -1,6 +1,19 @@
 
-function submitUsername() {
-    let inputField = document.getElementById("inputUsername").textContent;
-    sessionStorage.setItem("username", inputField);
-    document.getElementById("inputUsername").innerHTML = sessionStorage.getItem("username") + "dwdwd";
+function fillUsernameInput() {
+    if(localStorage.getItem("username")){
+        let inputField = <HTMLInputElement> document.getElementById("inputUsername");
+        inputField.value = localStorage.getItem("username");
+    }
 }
+
+function submitUsername() {
+    let inputField = <HTMLInputElement> document.getElementById("inputUsername");
+    localStorage.setItem("username", inputField.value);
+    document.getElementById("inputUsername").innerHTML = localStorage.getItem("username");
+}
+
+function getUsername() {
+    return localStorage.getItem("username");
+}
+
+fillUsernameInput();
